e48cc6da27f01a1dfcbd3a7eb16bbcc48b43df10
TheNigis <crismasaav@gmail.com>
2025-06-02 00:06:57 -0400

    cambios

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ const express = require('express');
 const flash   = require('express-flash');
 const app     = express();
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '500mb' }));
+app.use(express.urlencoded({ limit: '500mb', extended: true }));
 app.use(flash());
 
 if (process.env.MONGO_USER != 0){
